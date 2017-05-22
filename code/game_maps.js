8f70225bc1c5cece8892400312e40e5810272889
djc19970510 <1210316335@qq.com>
2017-05-22 18:30:50 +0800

    beta0.01

---
+++ code/game_maps.js
@@ -0,0 +1,29 @@
+var GAME_LEVELS = [
+  ["                                                                                ",
+   "                                                     oo                         ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                    ?                                                           ",
+   "                                                                                ",
+   "                                                                                ",
+   "                                                                                ",
+   "                ? x?x?x                     --         --                           ",
+   "                                    --      ||         ||                             ",
+   "                          --        ||      ||         ||                               ",
+   "        @                 ||        ||      ||         ||                               ",
+   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxx                                                       ",
+   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                       "]
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
